6d57cd09b142b54494efb6d9048d7037181f85f9
Altynai_UG <altynai.dzhusupovaa@gmail.com>
2023-10-11 19:20:19 +0600

    navbar

---
+++ src/components/Navbar/profileMenu.jsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import "./Navbar.css";
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        className="profileFlex"
+      >
+        <MenuIcon />
+        <AccountCircleIcon />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+        sx={{ borderRadius: "2rem" }}
+      >
+        <MenuItem onClick={handleClose}>Sign up</MenuItem>
+        <MenuItem onClick={handleClose}>Sign in</MenuItem>
+        <div
+          style={{ height: "1px", backgroundColor: "grey", width: "100%" }}
+        />
+        <MenuItem onClick={handleClose}>Airbnb Your Home</MenuItem>
+        <MenuItem onClick={handleClose}>Host an experience</MenuItem>
+        <MenuItem onClick={handleClose}>Help</MenuItem>
+      </Menu>
+    </div>
+  );
+}
