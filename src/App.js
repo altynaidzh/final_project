563e8b878efb047343ba3683592f52a874491302
Altynai_UG <altynai.dzhusupovaa@gmail.com>
2023-10-18 19:11:55 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import Routing from "./Routing";
+import CardList from "./components/CardList/CardList";
+import FilterM from "./components/FilterM";
+import FooterM from "./components/FooterM/FooterM";
 import Navbar from "./components/Navbar";
 import AuthContextProvider from "./context/authContext";
 import ProductsContextProvider from "./context/productContext";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   console.log("APP");
@@ -11,7 +15,10 @@ function App() {
       <ProductsContextProvider>
         <AuthContextProvider>
           <Navbar />
+          <FilterM />
+
           <Routing />
+          <FooterM />
         </AuthContextProvider>
       </ProductsContextProvider>
     </>
